f5129499321d52b8e904a18964db36e89421dda6
hp <niranjan962796@gmail.com>
2024-07-12 14:55:12 +0530

    second commit

--- src/NETFLIX/Cartun.js
+++ src/NETFLIX/Cartun.js
@@ -2,6 +2,8 @@ import "./Cartun.css";
 
 const Cartun=()=>{
     return(
+        <div>
+            <div className="cortune_hr"></div>
         <div className="cartun-box" >
         <div className="cartun-flexbox">
             <div className="cartun-1">
@@ -10,9 +12,15 @@ const Cartun=()=>{
                 <h1 className="cartun-h" >Create profiles for kids</h1>
                 <p className="cartun-p">Send children on adventures withe made just </p>
                 <p className="cartun-p">space made just for them—free with your membership.</p>
+
             </div>
+            
         </div>
+        
         </div>
+        <div className="cortune_hr"></div>
+        </div>
+        
     )
 }
 export default Cartun;
